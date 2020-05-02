be3458b0e44038a3f4851c7725814cc331cf9697
marysia <m.hayder@onet.eu>
2020-05-02 13:22:54 +0200

    zad1_1

---
+++ main.py
@@ -0,0 +1,34 @@
+import sqlite3
+from fastapi import FastAPI, Query
+
+{
+        "TrackId": int,
+        "Name": str,
+        "AlbumId": int,
+        "MediaTypeId": int,
+        "GenreId": int,
+        "Composer": str,
+        "Milliseconds": int,
+        "Bytes": int,
+        "UnitPrice": float
+}
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    app.db_connection = sqlite3.connect('chinook.db')
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    app.db_connection.close()
+
+
+@app.get("/tracks/{page}{per_page}")
+async def root(page: int = 0, per_page: int = 10):
+    cursor = app.db_connection.cursor()
+    data = app.db_connection.execute("SELECT * FROM tracks ORDER BY TrackId LIMIT per_page OFFSET page*per_page").fetchall()
+    return data
+
